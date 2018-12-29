4be791d6db6de3211bd7dfebfc7b3e5240ce073e
Joey Berger <joeyb@unr.edu>
2018-12-29 10:25:03 -0800

    functions and tests written

--- contracts/starNotary.sol
+++ contracts/starNotary.sol
@@ -11,11 +11,12 @@ contract StarNotary is ERC721 {
 //  Add a name and a symbol for your starNotary tokens
 
 //
-
     mapping(uint256 => Star) public tokenIdToStarInfo;
     mapping(uint256 => uint256) public starsForSale;
 
     function createStar(string _name, uint256 _tokenId) public {
+        require(bytes(_name).length > 0);
+
         Star memory newStar = Star(_name);
 
         tokenIdToStarInfo[_tokenId] = newStar;
@@ -23,7 +24,9 @@ contract StarNotary is ERC721 {
         _mint(msg.sender, _tokenId);
     }
 
-    function lookUptokenIdToStarInfo(uint256 _tokenId) public returns (string) {
+    function lookUptokenIdToStarInfo(uint256 _tokenId) public view returns (string) {
+        //make sure star exists
+        require(bytes(tokenIdToStarInfo[_tokenId].name).length > 0);
         return tokenIdToStarInfo[_tokenId].name;
     }
 
@@ -53,16 +56,29 @@ contract StarNotary is ERC721 {
 
 // Add a function called exchangeStars, so 2 users can exchange their star tokens...
 //Do not worry about the price, just write code to exchange stars between users.
-    function exchangeStars() public {
-
+    function exchangeStars(uint256 _firstUserTokenId, uint256 _secondUserTokenId) public {
+        //make sure both stars exist
+        require(bytes(tokenIdToStarInfo[_firstUserTokenId].name).length > 0);
+        require(bytes(tokenIdToStarInfo[_secondUserTokenId].name).length > 0);
+        //make sure token ids are unique
+        require(_firstUserTokenId != _secondUserTokenId, "Token Ids are the same");
+        //store in memory the first user's star
+        Star memory tempStar = tokenIdToStarInfo[_firstUserTokenId];
+        tokenIdToStarInfo[_firstUserTokenId] = tokenIdToStarInfo[_secondUserTokenId];
+        tokenIdToStarInfo[_secondUserTokenId] = tempStar;
     }
 //
 
 // Write a function to Transfer a Star. The function should transfer a star from the address of the caller.
 // The function should accept 2 arguments, the address to transfer the star to, and the token ID of the star.
 //
-        function transferStar(address _transferAddress, uint256 _tokenId) public {
-        //make sure that the star is valid at tokenId
-        
+    function transferStar(address _transferAddress, uint256 _tokenId) public {
+        //make sure star exists
+        require(bytes(tokenIdToStarInfo[_tokenId].name).length > 0);
+        //make sure star is not getting transferred to self
+        require(_transferAddress != msg.sender);
+        //make transfer
+        _removeTokenFrom(msg.sender, _tokenId);
+        _addTokenTo(_transferAddress, _tokenId);
     }
 }
